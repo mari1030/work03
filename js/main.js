11daa6fdb39346ce09c91c6c26829240362870fc
mari1030 <mariyuuto1211@gmail.com>
2021-04-27 20:10:08 +0900

    js課題①-2

--- js/main.js
+++ js/main.js
@@ -1,12 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
   const inputTask = document.getElementById('js-input');
   const inputButton = document.getElementById('js-input-button');
+  const deleteButton = document.getElementById('js-delete-button');
   const todos = [];
 
   inputButton.addEventListener('click', function(){
     
-    if(inputTask.value === "" || inputTask.value === undefined || inputTask.value === null) {
+    if(inputTask.value === '' || inputTask.value === undefined || inputTask.value === null) {
       alert("タスクを入力してください");
+      return;
     }
 
     const todo = addTodo(inputTask.value);
@@ -16,9 +18,35 @@ document.addEventListener('DOMContentLoaded', function() {
     displayTodos(todos);
     
     inputTask.value = "";
+
   });
 });
 
+// 削除ボタンを押下したときにその一列を削除する
+function deleteTr(e) {
+  // 押下された削除ボタンの列のid
+  const trId = e.target.id;
+  
+  // 押下された削除ボタンの列のtr
+  const targetTr = document.getElementById(`tr${trId}`);
+  // 押下された削除ボタンの列のtrを消去
+  targetTr.remove();
+
+  updateIndex();
+}
+
+// 削除ボタンを押下した際にindexの順をふり直す
+function updateIndex() {
+  // tr要素を取得（idがついているもののみ）
+  const tableElement = document.querySelectorAll('[id^="tr"]');
+
+  // indexを振り直す
+  tableElement.forEach(function(element, index) {
+    element.firstChild.innerHTML = index;
+  });
+}
+
+// todosの要素todoオブジェクトにタスクを追加
 function addTodo(task) {
   const todo = {
     task: task,
@@ -27,26 +55,47 @@ function addTodo(task) {
   return todo;
 }
 
+// 追加されたタスクを画面に表示する
 function displayTodos(array) {
     const table = document.getElementById('js-table');
     const tableElement = document.createElement('tr');
+    const tableTdStatus = document.createElement('td');
+    const tableTdDelete = document.createElement('td');
 
     const statusElement = createStatusBtn();
-    const deleteElement = createDeleteBtn();
+    const deleteElement = createDeleteBtn(array);
 
-    tableElement.innerHTML = `<td>${array.length - 1}</td><td>${array[array.length - 1].task}</td><td>${statusElement.outerHTML}</td><td>${deleteElement.outerHTML}</td>`;
+    array.forEach(function(element, index) {
+      tableElement.innerHTML = `<td>${index}</td><td>${element.task}</td>`;
+    });
 
     table.appendChild(tableElement);
+    tableElement.id = `tr${array.length - 1}`;
+    // 末尾にtd作成
+    tableElement.appendChild(tableTdStatus);
+    // 作業中ボタン追加
+    tableTdStatus.appendChild(statusElement);
+    // 末尾にtd作成
+    tableElement.appendChild(tableTdDelete);
+    // 削除ボタン追加
+    tableTdDelete.appendChild(deleteElement);
+    deleteElement.id = array.length - 1;
 }
 
+// 作業中ボタンを作成する
 function createStatusBtn() {
   const statusElement = document.createElement('button');
   statusElement.innerText = '作業中';
   return statusElement;
 }
 
-function createDeleteBtn() {
+// 削除ボタンを作成する
+function createDeleteBtn(todos) {
   const deleteElement = document.createElement('button');
   deleteElement.innerText = '削除';
+
+  // 削除ボタンにクリック関数をもたせる、関数の引数はarrayのtodos
+  deleteElement.addEventListener('click' , {name: todos, handleEvent: deleteTr}, false);
+
   return deleteElement;
 }
